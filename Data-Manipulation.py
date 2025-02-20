0077825570a7a770ef9917745abf160e379ec88d
se-mason <s.mason0649@outlook.com>
2025-02-20 11:21:24 +0000

    created bounds function to allow for bounds to be set for datapoints. This is a seperate function to allow for change in the future

--- Data-Manipulation.py
+++ Data-Manipulation.py
@@ -21,3 +21,18 @@ column_names = [info[1] for info in columns_info]
 
 # Remove the first two columns (index and date)
 column_names = column_names[2:]
+
+
+def boundsFunction(DF_range, deviationWeight):
+    '''Fucntion to calcualte the upper and lowerbounds of accepted values'''
+
+    # Calculate the mean and standard deviation of the DataFrame
+    deviation = DF_range.std(axis=0, skipna=True, ddof=1, numeric_only=True)
+    average = DF_range.mean(axis=0, skipna=True, numeric_only=True)
+
+    # Calculate the upper and lower bounds
+    lower_bound = average.iloc[1] - (deviationWeight * deviation.iloc[1])
+    upper_bound = average.iloc[1] + (deviationWeight * deviation.iloc[1])
+
+    # Return the bounds
+    return lower_bound, upper_bound
